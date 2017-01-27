f7ca28f460ab343ff97783ed5a7f8e2dc90d1474
ramseyboy <awahannan@gmail.com>
2017-01-27 15:37:21 -0500

    Extract coordinates from map item anchor tag

--- scraper/converter.ts
+++ scraper/converter.ts
@@ -1,5 +1,5 @@
 import * as cheerio from "cheerio"
-import * as fs from "fs"
+import {Url, parse} from "url"
 
 const SWITCH_COLUMN = 4
 
@@ -20,16 +20,47 @@ export class Converter {
             matrix[i] = new Array<string>()
 
             htmlTable(row).find('th').each((j, head) => {
-                matrix[i][j] = htmlTable(head).text().trim().replace(/(\r\n|\n|\r)/gm, "")
+                let headerText = htmlTable(head).text().trim().replace(/(\r\n|\n|\r)/gm, "")
+                if (headerText === 'Map') {
+                    matrix[i][j] = 'Lat'
+                    matrix[i][j+1] = 'Lng'
+                } else {
+                    matrix[i][j] = headerText
+                }
             });
 
             htmlTable(row).find('td').each((j, data) => {
-                matrix[i][j] = htmlTable(data).text().trim()
+                let innerText = htmlTable(data).text().trim()
+                if (innerText === 'map') {
+                    let coordsUrl: string = htmlTable(data).children().attr('href')
+                    if (coordsUrl) {
+                        let latlngMap: Map<string, string> = this.extractCoords(coordsUrl)
+                        matrix[i][j] = latlngMap.get("lat")
+                        matrix[i][j+ 1] = latlngMap.get("lng")
+                    } else {
+                        matrix[i][j] = ''
+                    }
+                } else {
+                    matrix[i][j] = innerText
+                }
             });
         });
 
         //filter out items that do not contain a switch identifier
         return matrix.filter(row => row[SWITCH_COLUMN].length)
+    }
+
+    private extractCoords(coordsUrl: string): Map<string, string> {
+        let urlParts: Url = parse(coordsUrl, true);
+        let loc: string = urlParts.query['q']
+        let coords: string = loc.split(":")[1]
+        
+        let latlngArr: string[] = coords.split(",")
+
+        let latlng: Map<string, string> = new Map<string, string>();
+        latlng.set("lat", latlngArr[0])
+        latlng.set("lng", latlngArr[1])
+        return latlng
     }
 
     private mapToCsv(table: Array<Array<string>>): string {
