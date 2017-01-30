7e5bdf718c367452bb44f72791ca0380a7e1c89a
ramseyboy <awahannan@gmail.com>
2017-01-30 09:07:02 -0500

    Convert main callback style code to promises

--- scraper/main.ts
+++ scraper/main.ts
@@ -2,24 +2,51 @@ import * as fs from "fs"
 import * as csv2geojson from 'csv2geojson'
 import {Converter} from "./converter"
 
-function main() {
-    fs.readFile("resources/434.html", "utf-8", function (err, data) {
-        if (err) throw err
+class App {
 
-        let converter = new Converter()
-        let csv = converter.convert(data)
+    private readonly converter = new Converter()
 
-        let geoJson = csv2geojson.csv2geojson(csv, function(err, data) {
-            if (err) {
-                console.log(err)
-                throw err
-            }
-
-            let json = JSON.stringify(data, null, 2)
+    public async run() {
+        try {
+            let html: string = await this.readFile("resources/434.html")
+            let csv: string = this.converter.convert(html)
+            let geoJson: Object =  await this.convertCsvToGeoJson(csv)
+            let json: string = JSON.stringify(geoJson, null, 2)
             console.log(json)
             // fs.writeFile("resources/434.json", json)
-        });
-    })
+            
+        } catch(err) {
+            console.log(`error: ex ->`, err)
+        }
+    }
+
+    private readFile(filePath: string): Promise<string> {
+        return new Promise(function(resolve, reject) {
+            fs.readFile(filePath, "utf-8", function(err, data) {
+                if(err !== null) {
+                    return reject(err);
+                }
+
+                resolve(data);
+            })
+        })
+    }
+
+    private convertCsvToGeoJson(csv: string): Promise<Object> {
+        return new Promise(function(resolve, reject) {
+            csv2geojson.csv2geojson(csv, function(err, data) {
+                if (err) {
+                    return reject(err);
+                }
+
+                resolve(data)
+            })
+        })
+    }
+}
+
+function main() {
+    new App().run()
 }
 
 main()
