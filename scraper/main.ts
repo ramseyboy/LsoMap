238cf5471972367de67b16be1c3148aadff8c72a
ramseyboy <awahannan@gmail.com>
2017-01-30 20:06:37 -0500

    Add pagination code to scraper

--- scraper/main.ts
+++ scraper/main.ts
@@ -1,22 +1,68 @@
 import * as fs from "fs"
 import * as csv2geojson from 'csv2geojson'
 import {Converter} from "./converter"
+import axios, {
+  AxiosResponse
+} from "axios"
 
 class App {
 
+    private static readonly url: string = "http://localcallingguide.com/lca_prefix.php"
+    private static readonly out_dir: string = "out/"
     private readonly converter = new Converter()
 
     public async run() {
         try {
-            let html: string = await this.readFile("resources/434.html")
-            let csv: string = this.converter.convert(html)
-            let geoJson: Object =  await this.convertCsvToGeoJson(csv)
-            let json: string = JSON.stringify(geoJson, null, 2)
-            console.log(json)
-            // fs.writeFile("resources/434.json", json)
+            if (!fs.existsSync(App.out_dir)) {
+                fs.mkdirSync(App.out_dir)
+            }
+
+            let areacodes: string = await this.readFile("resources/areacodes.csv")
+            for (let line of areacodes.split("\n")) {
+                let areaCode = line.split(",")[0]
+                if (Number(areaCode)) {
+                    this.paginateAreaCodes(areaCode)
+                }                
+            }
             
         } catch(err) {
             console.log(`error: ex ->`, err)
+            process.exit(1)
+        }
+    }
+
+    private async paginateAreaCodes(areaCode: string) {
+        try {
+            var index = 1
+            while (true) {
+                let html: AxiosResponse = await axios.get(App.url, {
+                    params: {
+                        page: index,
+                        npa: areaCode
+                    }
+                })
+
+                let csv: string = this.converter.convert(html.data)
+                let geoJson = await this.convertCsvToGeoJson(csv)
+                // if we dont have any valid features, we are out of pages
+                if (geoJson.features.length == 0) {
+                    break
+                }
+                
+                let json: string = JSON.stringify(geoJson, null, 2)
+                
+                let areaCodeDir = App.out_dir + areaCode
+                if (!fs.existsSync(areaCodeDir)) {
+                    fs.mkdirSync(areaCodeDir)
+                }
+
+                fs.writeFileSync(`${areaCodeDir}/${areaCode}_${index}.json`, json)
+
+                index++
+            }
+        } catch(err) {
+            console.log(`pagination error: ex ->`, err)
+            process.exit(1)
         }
     }
 
@@ -32,7 +78,7 @@ class App {
         })
     }
 
-    private convertCsvToGeoJson(csv: string): Promise<Object> {
+    private convertCsvToGeoJson(csv: string): Promise<any> {
         return new Promise(function(resolve, reject) {
             csv2geojson.csv2geojson(csv, function(err, data) {
                 if (err) {
