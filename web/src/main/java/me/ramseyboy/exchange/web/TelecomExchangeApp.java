bd40ebb0f25955e7b3bb097bdf1f3fd42b5821a5
ramseyboy <awahannan@gmail.com>
2017-02-24 13:38:36 -0500

    Split web app into its own gradle module

--- web/src/main/java/me/ramseyboy/exchange/web/TelecomExchangeApp.java
+++ web/src/main/java/me/ramseyboy/exchange/web/TelecomExchangeApp.java
@@ -1,4 +1,4 @@
-package me.ramseyboy.exchange;
+package me.ramseyboy.exchange.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
