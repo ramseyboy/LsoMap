a978e0c55f1d498166fd3cadfe4ce0cd7fb26fbb
ramseyboy <awahannan@gmail.com>
2017-02-24 11:36:50 -0500

    Create scripts for init and loading data into DB

--- scraper/scripts/collect-json.py
+++ scraper/scripts/collect-json.py
@@ -1,15 +1,60 @@
 #! /usr/bin/env python3
 
+import json
+from argparse import ArgumentParser, FileType
+from re import compile
 import os
-import sys
 
-jsonDir = "../out"
+float_pat = compile(r'^-?\d+\.\d+(e-?\d+)?$')
+charfloat_pat = compile(r'^[\[,\,]-?\d+\.\d+(e-?\d+)?$')
 
-outFilePath = "../out/collected-switches.json"
-outFile = open(outFilePath, 'w+')
+parser = ArgumentParser(description="Group (merge) multiple GeoJSON files.")
 
-for root, subdirs, files in os.walk(jsonDir):
-    for filePath in files:
-        jsonFile = open(root + "/" + filePath)
-        outFile.write(jsonFile.read())
-    
+defaults = dict(precision=6)
+
+parser.set_defaults(**defaults)
+
+parser.add_argument('-p', '--precision', dest='precision',
+                    type=int, help='Digits of precision')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    jsonDir = "../out"
+
+    outjson = dict(type='FeatureCollection', features=[])
+
+    for root, subdirs, files in os.walk(jsonDir):
+        for infile in files:
+            injson = json.loads(open(root + "/" + infile).read())
+
+            if injson.get('type', None) != 'FeatureCollection':
+                raise Exception('Sorry, "%s" does not look like GeoJSON' % infile)
+
+            if type(injson.get('features', None)) != list:
+                raise Exception('Sorry, "%s" does not look like GeoJSON' % infile)
+            try:
+                outjson['features'] += injson['features']
+            except:
+                outjson['features'] += injson
+
+    encoder = json.JSONEncoder(separators=(',', ':'))
+    encoded = encoder.iterencode(outjson)
+
+    format = '%.' + str(args.precision) + 'f'
+
+    outFilePath = "../out/collected-switches.json"
+    outFile = open(outFilePath, 'w+')
+    output = outFile
+
+    for token in encoded:
+        if charfloat_pat.match(token):
+            # in python 2.7, we see a character followed by a float literal
+            output.write(token[0] + format % float(token[1:]))
+
+        elif float_pat.match(token):
+            # in python 2.6, we see a simple float literal
+            output.write(format % float(token))
+
+        else:
+            output.write(token)
---
+++ scraper/scripts/initialize_database.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+psql -f create.sql
+psql -d exchange -f extensions.sql
+ogr2ogr -f "PostgreSQL" PG:"dbname=exchange user=exchangeapp" "../resources/collected-switches.json" -nln switches -append
+ogr2ogr -f "PostgreSQL" PG:"dbname=exchange user=exchangeapp" "../resources/areacodes.json" -nln area_codes -append
