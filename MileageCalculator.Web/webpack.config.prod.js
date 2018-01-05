0f7dd20590ed5ee44a1a94faa1989d3f3b022795
Walker Hannan <awahannan@gmail.com>
2018-01-05 15:02:28 -0600

    Add initial blazor frontend and add scraper to sln

--- MileageCalculator.Web/webpack.config.prod.js
+++
@@ -1,8 +0,0 @@
-var webpack = require('webpack');
-
-module.exports = {
-    plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({minimize: true})
-    ]
-};
--- MileageCalculator.Web/webpack.consts.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    CLIENT_DIR: "src-client",
-    OUTPUT_DIR: "src/public",
-    OUTPUT_ARTIFACT: "bundle",
-    isDevelopment: function(env) {
-        return env.debug === 'true';
-    }
-};
